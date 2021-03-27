f717a3eaafefc8cf1968b11512b7030a419a14c7
SAAF-16 <samsnow16@gmail.com>
2021-03-27 14:07:35 +0100

    added full keyboard support and formatting

--- script/javascript.js
+++ script/javascript.js
@@ -1,7 +1,7 @@
 let operand1 = "0"
 let operand2 = "0"
 
-let isNumber = /[0-9]/ //RegEx to identify if it's a number
+let isNumber = /[0-9\.]/ //RegEx to identify if it's a number
 let deleteKeys = ['Backspace', 'Delete', 'del'];
 
 const calcScreen = document.getElementById('calcScreen')
@@ -14,30 +14,34 @@ operatorsDiv.addEventListener('click', e => {
     const isButton = e.target.nodeName === 'BUTTON';
     if (!isButton) return;//if i'm not clicking a button 
     if (deleteKeys.includes(e.target.id)) {
-        deleteNumber();  
+        deleteNumber();
     }
 
     updateScreen();
 })
 
 numbersDiv.addEventListener('click', (e) => {
+    console.log(e);
     const isButton = e.target.nodeName === 'BUTTON';
     if (!isButton) {  //if i'm not clicking the button
         return;
     }
-        if (operand1 == "0") operand1 = "";
-        operand1 += e.target.id.slice(-1);
+    if (doublePoint(e.target.textContent)) return false
+    if (operand1 == "0") operand1 = "";
+    console.log(e.target.textContent)
+    operand1 += e.target.textContent;
     updateScreen();
 })
 
 window.addEventListener("keydown", e => {
-    if (deleteKeys.includes(e.code)) {
-        deleteNumber()
+    if (doublePoint(e.key)) return false
+    if (deleteKeys.includes(e.key)) {
+        deleteNumber();
     }
-    else if (!(isNumber.test((e.code.slice(-1))))) return false
+    else if (!(isNumber.test((e.key)))) return false
     else {
         if (operand1 == "0") operand1 = "";
-        operand1 += e.code.slice(-1);
+        operand1 += e.key;
     }
     updateScreen();
 });
@@ -60,13 +64,19 @@ window.addEventListener("keydown", e => {
 
 
 
+function doublePoint(e) {
+    return [...operand1].includes('.') && e == '.';
+}
+
 function deleteNumber() {
     operand1 = operand1.slice(0, -1);
     if (operand1 == "") operand1 = "0"
 }
 
 function updateScreen() {
-    calcScreen.textContent = operand1;
+    calcScreen.textContent = operand1.toString();
+    calcScreen.scrollLeft = calcScreen.scrollWidth
+    console.log(operand1)
 }
 
 function operate(operand1, operator, operand2) {
