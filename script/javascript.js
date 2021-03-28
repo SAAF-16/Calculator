99aabb5e657a6dbe9fc74cb784c8369895fc30f7
SAAF-16 <samsnow16@gmail.com>
2021-03-28 11:43:19 +0200

    solved del button bug

--- script/javascript.js
+++ script/javascript.js
@@ -1,13 +1,11 @@
 let activeNumber = "0";
-
-
 let operandone = "";
 let operator = "";
 let operandtwo = "";
 let operatorSimbol = "";
 
 let isNumber = /[0-9\.]/;  //RegEx to identify if it's a number
-let isOperator = /[/*\-\+%!^÷×√]/;
+let isOperator = /[/*\-\+%!^÷×√]/; //RegEx to identify if it's an operator
 let deleteKeys = ['Backspace', 'Delete', 'del'];
 let resultKeys = ['Enter', '='];
 
@@ -16,103 +14,91 @@ const calcScreenTop = document.getElementById('calcScreenTop');
 const underScreen = document.getElementById('under-screen');
 const numbersDiv = document.getElementById('numbers-div');
 const operatorsDiv = document.getElementById('operators-div');
+
+operatorsDiv.addEventListener('click', manageOperatorsButtons());
+numbersDiv.addEventListener('click', manageNumbersButtons());
+numbersDiv.addEventListener('keydown', avoidEnterKeyDefault());
+window.addEventListener("keydown", manageKeyboardBehaviour());
+
 updateScreen();
 
-operatorsDiv.addEventListener('click', e => {
-    if (notButton(e)) return false;         //if i'm not clicking a button 
-    if (isDeleteKey(e.target.id)) deleteDigit();
-    if (e.target.id == 'ac') {
-        activeNumber = "0";
+
+
+
+
+function manageKeyboardBehaviour() {
+    return e => {
+        if (calcScreenTop.textContent.slice(-1) == '=') resetCalculator();
+        if (doublePoint(e.key)) return false;
+        if (isDeleteKey(e.key)) deleteDigit();
+        else if (resultKeys.includes(e.key)) finalResult();
+
+        else if (isOperator.test(e.key)) {
+            if (calcScreenTop.textContent != "") {
+                nextResultCalculation(e.key);
+                updateScreen();
+                return false;
+            }
+            updateOperation(e.key);
+        }
+        else if (!(isNumber.test((e.key)))) return false;
+
+        else {
+            lastDigit = activeNumber.slice(-1);
+            if (isOperator.test(lastDigit)) {
+                calcScreenTop.textContent = activeNumber;
+                activeNumber = '';
+            }
+            if (activeNumber == "0") activeNumber = "";
+            activeNumber += e.key;
+        }
         updateScreen();
-        return false;
-    }
+    };
+}
 
-    else if (calcScreenTop.textContent != "") {
-        nextResultCalculation(e.target.id);
+function manageNumbersButtons() {
+    return (e) => {
+        if (calcScreenTop.textContent.slice(-1) == '=') resetCalculator();
+        if (notButton(e)) return false;
+        if (doublePoint(e.target.textContent)) return false;
+        if (resultKeys.includes(e.target.textContent)) finalResult();
+        else if (isNumber.test(e.target.id)) {
+            lastDigit = activeNumber.slice(-1);
+            if (e.target.textContent == 'x10') {
+                activeNumber = (activeNumber * 10).toString();
+            }
+            else if (isOperator.test(lastDigit)) {
+                calcScreenTop.textContent = activeNumber;
+                activeNumber = '';
+            }
+            if (activeNumber == "0") activeNumber = "";
+            activeNumber += e.target.textContent;
+        }
         updateScreen();
-        return false;
-    } else {
-        updateOperation(e.target.id);
-    }
-    updateScreen();
-});
+    };
+}
 
-numbersDiv.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') e.preventDefault();
-});
-numbersDiv.addEventListener('click', (e) => {
-    console.log(e);
-    if (calcScreenTop.textContent.slice(-1) == '=') resetCalculator();
-    if (notButton(e)) return false;
-    if (doublePoint(e.target.textContent)) return false;
-    if (resultKeys.includes(e.target.textContent)) {
-        console.log('sto qua');
-        finalResult();
-    }
-    else if (isNumber.test(e.target.id)) {
-        lastDigit = activeNumber.slice(-1);
-
-
-        if (e.target.textContent == 'x10') {
-            activeNumber = (activeNumber * 10).toString();
-        } else if (isOperator.test(lastDigit)) {
-            calcScreenTop.textContent = activeNumber;
-            activeNumber = '';
+function manageOperatorsButtons() {
+    return e => {
+        if (notButton(e)) return false; //if i'm not clicking a button 
+        if (isDeleteKey(e.target.id)) deleteDigit();
+        else if (e.target.id == 'ac') {
+            activeNumber = "0";
         }
-        if (activeNumber == "0") activeNumber = "";
-        activeNumber += e.target.textContent;
-    }
-    updateScreen();
-});
-
-window.addEventListener("keydown", e => {
-    // console.log(e.key);
-    if (calcScreenTop.textContent.slice(-1) == '=') resetCalculator();
-    if (doublePoint(e.key)) return false;
-    if (isDeleteKey(e.key)) deleteDigit();
-    if (resultKeys.includes(e.key)) finalResult();
-
-
-    else if (isOperator.test(e.key)) {
-        if (calcScreenTop.textContent != "") {
-            nextResultCalculation(e.key);
-            updateScreen();
-            return false;
+        else if (calcScreenTop.textContent != "") {
+            nextResultCalculation(e.target.id);
+        } else {
+            updateOperation(e.target.id);
         }
-        updateOperation(e.key);
-    }
+        updateScreen();
+    };
+}
 
-    else if (!(isNumber.test((e.key)))) return false;
-
-    else {
-        lastDigit = activeNumber.slice(-1);
-        if (isOperator.test(lastDigit)) {
-            calcScreenTop.textContent = activeNumber;
-            activeNumber = '';
-        }
-        if (activeNumber == "0") activeNumber = "";
-        activeNumber += e.key;
-    }
-    updateScreen();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function avoidEnterKeyDefault() {
+    return (e) => {
+        if (e.key == 'Enter') e.preventDefault();
+    };
+}
 
 function finalResult() {
     operandtwo = activeNumber;
@@ -204,12 +190,9 @@ function deleteDigit() {
 function updateScreen() {
     calcScreenBottom.textContent = activeNumber.toString();
     calcScreenBottom.scrollLeft = calcScreenBottom.scrollWidth;
-    console.log(activeNumber);
 }
 
 function operate(op1, operator, op2) {
-    console.log(op1);
-    console.log(op2);
     return operator(op1, op2);
 }
 
@@ -236,9 +219,11 @@ function power(a, b) {
 function mod(a, b) {
     return a % b;
 }
+
 function sqrt(a, b) {
     return a ** (1 / b);
 }
+
 function factorial(a) {
     if (a == 0) return 1;
     total = 1;
